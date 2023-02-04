139b04bbdd4889d819ad167bc114593c6cfab4ad
Ziad <ziad.abtar@montymobile.com>
2023-02-04 22:14:31 +0200

    adding character epoxy model with relative extension function for building proper image url from the thumbnail object

--- app/src/main/java/com/ziad/marvelmdi/presentation/characters/CharacterItemModel.kt
+++ app/src/main/java/com/ziad/marvelmdi/presentation/characters/CharacterItemModel.kt
@@ -1,30 +1,47 @@
 package com.ziad.marvelmdi.presentation.characters
 
+import android.content.Context
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import com.airbnb.epoxy.EpoxyAttribute
 import com.airbnb.epoxy.EpoxyHolder
 import com.airbnb.epoxy.EpoxyModelClass
 import com.airbnb.epoxy.EpoxyModelWithHolder
+import com.bumptech.glide.Glide
 import com.ziad.marvelmdi.R
+import com.ziad.marvelmdi.presentation.core.BaseEpoxyHolder
+import com.ziad.marvelmdi.utils.getUsableUrl
 
 @EpoxyModelClass(layout = R.layout.item_character)
-abstract class MovieItemModel : EpoxyModelWithHolder<MovieItemModel.Holder>() {
+abstract class CharacterItemModel : EpoxyModelWithHolder<CharacterItemModel.Holder>() {
 
     @EpoxyAttribute
     lateinit var character: com.ziad.marvelmdi.data.remote.model.Character
 
     override fun bind(holder: Holder) {
+        super.bind(holder)
+        with(holder) {
+            tvName.text = character.name
+            tvDesc.text = character.description
 
-        holder.titleView?.text = character.name
+            if (character.description.isNullOrEmpty())
+                tvDesc.visibility = View.GONE
+            else
+                tvDesc.visibility = View.VISIBLE
+
+            Glide
+                .with(tvName.context)
+                .load(character.thumbnail.getUsableUrl())
+                .placeholder(R.color.grey)
+                .into(ivCharacter);
+        }
     }
 
-    class Holder : EpoxyHolder() {
-        var titleView: TextView? = null
-
-        override fun bindView(itemView: View) {
-            titleView = itemView.findViewById(R.id.tv_hero_name)
-        }
+    class Holder : BaseEpoxyHolder() {
+        val tvName: TextView by bind(R.id.tv_name)
+        val ivCharacter: ImageView by bind(R.id.iv_character)
+        val tvDesc: TextView by bind(R.id.tv_desc)
 
     }
 }
--- app/src/main/java/com/ziad/marvelmdi/utils/Extensions.kt
+++ app/src/main/java/com/ziad/marvelmdi/utils/Extensions.kt
@@ -1,2 +1,7 @@
 package com.ziad.marvelmdi.utils
 
+import com.ziad.marvelmdi.data.remote.model.Thumbnail
+
+fun Thumbnail.getUsableUrl(): String {
+    return path.replace("http","https").plus(".$extension")
+}
