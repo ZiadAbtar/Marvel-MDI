a1e05a24e4540894f4ac37c8576a44f3069ac80b
Ziad <ziad.abtar@montymobile.com>
2023-02-05 11:37:11 +0200

    adding a comment to explain why public and private key are exposed

--- app/src/main/java/com/ziad/marvelmdi/utils/Constants.kt
+++ app/src/main/java/com/ziad/marvelmdi/utils/Constants.kt
@@ -1,6 +1,10 @@
 package com.ziad.marvelmdi.utils
 
 object Constants {
+
+    //PUBLIC_KEY and PRIVATE_KEY should be ideally be saved on server side
+    //And should be saved in build config if we need them locally to not be pushed to git
+    //however for this project, in order for the code to be easily installed and tested, they are totally exposed
     const val PUBLIC_KEY = "3eaee1fb89dace8b24cc4105397f3683"
     const val PRIVATE_KEY = "74cd00550d0b7364ac872a030d192054072e6a06"
 }
