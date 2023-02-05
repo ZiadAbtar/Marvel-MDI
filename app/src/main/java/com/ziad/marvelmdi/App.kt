4ce9663db2a49acf6463544551f511a18ecb3c5f
Ziad <ziad.abtar@montymobile.com>
2023-02-05 17:44:22 +0200

    disabling snapping behavior of carousel inside epoxy recycler view

--- app/src/main/java/com/ziad/marvelmdi/App.kt
+++ app/src/main/java/com/ziad/marvelmdi/App.kt
@@ -1,7 +1,13 @@
 package com.ziad.marvelmdi
 
 import android.app.Application
+import com.airbnb.epoxy.Carousel
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App : Application()
+class App : Application(){
+    override fun onCreate() {
+        super.onCreate()
+        Carousel.setDefaultGlobalSnapHelperFactory(null)
+    }
+}
