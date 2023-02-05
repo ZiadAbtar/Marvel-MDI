398d41619bb0c2c675078c7931d84cbcb56418f8
Ziad <ziad.abtar@montymobile.com>
2023-02-05 14:14:02 +0200

    setting name and ID to details screen as per the requirements

--- app/src/main/java/com/ziad/marvelmdi/presentation/character_details/CharacterDetailsFragment.kt
+++ app/src/main/java/com/ziad/marvelmdi/presentation/character_details/CharacterDetailsFragment.kt
@@ -5,6 +5,7 @@ import android.view.View
 import androidx.navigation.fragment.navArgs
 import androidx.transition.TransitionInflater
 import com.bumptech.glide.Glide
+import com.ziad.marvelmdi.R
 import com.ziad.marvelmdi.databinding.FragmentCharachterDetailsBinding
 import com.ziad.marvelmdi.presentation.core.BaseFragment
 import com.ziad.marvelmdi.utils.getUsableUrl
@@ -23,12 +24,17 @@ class CharacterDetailsFragment :
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val character = args.character
+
         binding.ivCharacter.apply {
-            val characterImage = args.character.thumbnail.getUsableUrl()
+            val characterImage = character.thumbnail.getUsableUrl()
             transitionName = characterImage
             Glide.with(this)
                 .load(characterImage)
                 .into(this)
         }
+
+        binding.tvName.text = character.name
+        binding.tvId.text = getString(R.string.id_s,character.id)
     }
 }
