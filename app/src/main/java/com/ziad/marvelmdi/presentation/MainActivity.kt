75ceb6c104dd8087b7556bcd52a516fa34ce078a
Ziad <ziad.abtar@montymobile.com>
2023-02-05 20:14:40 +0200

    setting action bar and linking it to the navigation component

--- app/src/main/java/com/ziad/marvelmdi/presentation/MainActivity.kt
+++ app/src/main/java/com/ziad/marvelmdi/presentation/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.ziad.marvelmdi.presentation
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.ziad.marvelmdi.R
 import com.ziad.marvelmdi.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,6 +19,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        val navController = findNavController(R.id.nav_main_flow)
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (item.itemId == android.R.id.home) {
+            findNavController(R.id.nav_main_flow).navigateUp()
+            true
+        } else
+            super.onOptionsItemSelected(item)
     }
 
 
