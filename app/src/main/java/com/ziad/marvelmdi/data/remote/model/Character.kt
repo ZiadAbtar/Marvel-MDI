606865be9e0c40d24078d93ff683aa8780bc89b6
Ziad <ziad.abtar@montymobile.com>
2023-02-05 14:06:13 +0200

    making charactr and thumbnail classes parcelable to send them between fragments

--- app/src/main/java/com/ziad/marvelmdi/data/remote/model/Character.kt
+++ app/src/main/java/com/ziad/marvelmdi/data/remote/model/Character.kt
@@ -1,8 +1,12 @@
 package com.ziad.marvelmdi.data.remote.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class Character(
     val id: Int,
     val name: String,
     val description: String,
     val thumbnail: Thumbnail
-)
+):Parcelable
--- app/src/main/java/com/ziad/marvelmdi/data/remote/model/Thumbnail.kt
+++ app/src/main/java/com/ziad/marvelmdi/data/remote/model/Thumbnail.kt
@@ -1,6 +1,10 @@
 package com.ziad.marvelmdi.data.remote.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class Thumbnail(
     val path: String,
     val extension: String
-)
+):Parcelable
